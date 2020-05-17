e3510b5e1ac32ca7bae2b9872d83079373a5b5ba
Scot Sexton <rss3509@gmail.com>
2020-05-17 01:49:39 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,10 +5,10 @@ def third_challenge
         name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        name: "Lady Montague", :age "53"
+        name: "Lady Montague", :age "54"
         },
       :hero => {
-        :name "Romeo", age: "15", status: "alive"
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
